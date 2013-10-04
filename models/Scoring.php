0e8469d55231580e2697fc8a3ebe027a6a3ebe2f
François Gannaz <francois.gannaz@silecs.info>
2013-10-04 15:41:34 +0200

    New model 'ScoringSystem'

---
+++ models/Scoring.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @license http://www.gnu.org/licenses/gpl-3.0.html  GNU GPL v3
+ */
+
+namespace mod\automultiplechoice;
+
+/**
+ * Scoring system
+ *
+ * @author François Gannaz <francois.gannaz@silecs.info>
+ */
+class ScoringSystem
+{
+    private $scorings = array(
+        'single' => array(),
+        'multiple' => array(),
+    );
+
+    /**
+     * Creates a new Scoring System from the module configuration in the DB.
+     *
+     * @return \mod\automultiplechoice\ScoringSystem
+     */
+    public static function createFromConfig()
+    {
+        $system = new self;
+        $raw = get_config('mod_automultiplechoice', 'scorings');
+        $lines = explode("\n", $raw);
+        foreach ($lines as $line) {
+            $cells = array_map('trim', explode("|", trim($line, " |")));
+            if (count($cells) === 4) {
+                $new = new Scoring();
+                $new->name = $cells[0];
+                $new->score = (double) $cells[1];
+                $new->multiple = (strcasecmp($cells[1], "multiple") === 0);
+                $new->formula = $cells[3];
+                $system->scorings[$new->multiple ? 'multiple' : 'single'][] = $new;
+            }
+        }
+        return $system;
+    }
+}
+
+class Scoring
+{
+    public $name = '';
+    public $score = 0;
+    public $multiple = false;
+    public $formula = '';
+}
