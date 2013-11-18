def57e883f33b2c47f3ed9f64398eedbeeb79bff
François Gannaz <francois.gannaz@silecs.info>
2013-11-18 21:44:43 +0100

    JS: jQuery code for async loading (untested)

---
+++ assets/async.js
@@ -0,0 +1,13 @@
+/* 
+ * @license http://www.gnu.org/licenses/gpl-3.0.html  GNU GPL v3
+ */
+
+console.log("Hello");
+$('.async-load').each(function(){
+	var url = $(this).data('url');
+	console.log(url);
+	$(this).children('.async-target').each(function(){
+		console.log($(this).data('parameters'));
+		$(this).load(url, $(this).data('parameters'));
+	});
+});
