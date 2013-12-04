e9e8c68118dc4bcfe6ff2dca1984ad9c7f7c35d1
François Gannaz <francois.gannaz@silecs.info>
2013-12-04 13:17:23 +0100

    M#2096 append scoring info to instructions

--- lib.php
+++ lib.php
@@ -73,6 +73,10 @@ function automultiplechoice_add_instance(stdClass $automultiplechoice, mod_autom
 
     $params = \mod\automultiplechoice\AmcParams::fromForm($automultiplechoice->amc);
     unset($automultiplechoice->amc);
+    $scoringset = \mod\automultiplechoice\ScoringSystem::read()->getScoringSet($params->scoringset);
+    if ($scoringset) {
+        $automultiplechoice->description .=  "\n" . $scoringset->description;
+    }
     $automultiplechoice->amcparams = $params->toJson();
     return $DB->insert_record('automultiplechoice', $automultiplechoice);
 }
