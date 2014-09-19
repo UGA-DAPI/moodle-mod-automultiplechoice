0f981d6f62ae77f9e16468bf98aaf717b15e3299
François Gannaz <francois.gannaz@silecs.info>
2014-09-19 15:41:13 +0200

    #2357 Use the controller in ajax/prepare

--- ajax/prepare.php
+++ ajax/prepare.php
@@ -1,34 +1,31 @@
 <?php
 
-/*
- * @license http://www.gnu.org/licenses/gpl-3.0.html  GNU GPL v3
+/**
+ * @package    mod_automultiplechoice
+ * @copyright  2014 Silecs
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-global $CFG;
+use \mod\automultiplechoice as amc;
 
-require_once dirname(dirname(dirname(__DIR__))) . '/config.php';
-require_once dirname(__DIR__) . '/models/Quizz.php';
+require_once dirname(__DIR__) . '/locallib.php';
 require_once dirname(__DIR__) . '/models/AmcProcessPrepare.php';
 
-$a  = optional_param('a', 0, PARAM_INT);  // automultiplechoice instance ID
+global $DB, $OUTPUT, $PAGE;
+/* @var $PAGE moodle_page */
+/* @var $OUTPUT core_renderer */
+
+$controller = new amc\Controller();
+$quizz = $controller->getQuizz();
+$cm = $controller->getCm();
+$course = $controller->getCourse();
+
+require_capability('mod/automultiplechoice:view', $controller->getContext());
+
 $action = optional_param('action', '', PARAM_ALPHANUMEXT);
 $redirect = optional_param('redirect', false, PARAM_BOOL);
 
-if ($a) {
-    $quizz = \mod\automultiplechoice\Quizz::findById($a);
-    $course     = $DB->get_record('course', array('id' => $quizz->course), '*', MUST_EXIST);
-    $cm         = get_coursemodule_from_instance('automultiplechoice', $quizz->id, $course->id, false, MUST_EXIST);
-} else {
-    require_login();
-    echo $OUTPUT->error_text('You must specify an instance ID');
-    exit();
-}
-
-require_login($course, true, $cm);
-$context = context_module::instance($cm->id);
-require_capability('mod/automultiplechoice:view', $context);
-
-$process = new \mod\automultiplechoice\AmcProcessPrepare($quizz);
+$process = new amc\AmcProcessPrepare($quizz);
 
 if ($action == 'prepare') {
     if ($process->amcCreatePdf("latex")) {
