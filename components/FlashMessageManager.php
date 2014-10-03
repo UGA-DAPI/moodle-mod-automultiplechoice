f992cc8ae84a0a3eef4e0c4879958aa7b63e8f4e
François Gannaz <francois.gannaz@silecs.info>
2014-10-03 16:47:25 +0200

    Add a flash-messages system

---
+++ components/FlashMessageManager.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package    mod_automultiplechoice
+ * @copyright  2014 Silecs
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod\automultiplechoice;
+
+class FlashMessageManager
+{
+    static public function init() {
+        self::clearMessages();
+    }
+
+    static public function addMessage($category, $message) {
+        global $SESSION;
+        $SESSION->flashmessages[$category][] = $message;
+    }
+
+    static public function displayMessages($clearAfterwards = true) {
+        global $SESSION;
+        global $OUTPUT;
+        if (empty($SESSION->flashmessages)) {
+            return;
+        }
+        foreach ($SESSION->flashmessages as $status => $messages) {
+            if ($messages) {
+                foreach ($messages as $message) {
+                    $class = ($status === 'error' ? "problem" : $status);
+                    echo $OUTPUT->notification($message, "notify" . $class);
+                }
+            }
+        }
+        if ($clearAfterwards) {
+            self::clearMessages();
+        }
+    }
+
+    static public function clearMessages() {
+        global $SESSION;
+        $SESSION->flashmessages = array(
+            'success' => array(),
+            'error' => array(),
+            'warning' => array(),
+        );
+    }
+}
