fa9fe0bf010ffde97c6795da72480175f9eac1e8
fabmen <menard.fabrice@gmail.com>
2015-03-18 10:00:32 +0100

    rewwrite

--- associating.php
+++ associating.php
@@ -10,7 +10,7 @@ use \mod\automultiplechoice as amc;
 
 require_once(__DIR__ . '/locallib.php');
 
-require_once __DIR__ . '/models/AmcProcessAssociate.php';
+require_once __DIR__ . '/models/AmcProcessUsers.php';
 
 global $DB, $OUTPUT, $PAGE;
 /* @var $DB moodle_database */
